6a0031783e9cf6e1fa46958c19610d42b72a19c4
omarkhaled-code <omarkhaled12oki@gmail.com>
2026-06-06 05:52:19 +0300

    handle application resposive on diffrenet devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
-import ToastContainer from "@/components/ui/Tost";
+import ToastContainer from "@/components/ui/Toast";
 import AuthProvider from "@/components/auth/AuthProvider"; // استيراد المكون الجديد
 import ConfirmModal from "@/components/ui/ConfirmModal";
 
--- components/dashboard/DashboardHeader.tsx
+++ components/dashboard/DashboardHeader.tsx
@@ -2,21 +2,35 @@
 
 import { usePathname } from 'next/navigation'
 import { useAuthStore } from '@/store/authStore'
+import { useRouter } from 'next/navigation'
 
 // Map routes to page titles
 const pageTitles: Record<string, string> = {
-  '/dashboard':           'Dashboard',
+  '/dashboard': 'Dashboard',
   '/dashboard/purchases': 'My Library',
-  '/dashboard/orders':    'Orders',
-  '/dashboard/settings':  'Settings',
-  '/dashboard/support':   'Support',
+  '/dashboard/orders': 'Orders',
+  '/dashboard/settings': 'Settings',
+  '/dashboard/support': 'Support',
 }
 
 export default function DashboardHeader() {
   const pathname = usePathname()
-  const { user } = useAuthStore()
+  const { user, clearAuth } = useAuthStore()
 
   const pageTitle = pageTitles[pathname] || 'Dashboard'
+
+  const router = useRouter()
+
+  const handleLogout = async () => {
+
+    const res = await fetch('/api/auth/logout', {
+      method: "POST"
+    })
+    if (res.ok) {
+      clearAuth()
+      router.push('/auth')
+    }
+  }
 
   return (
     <header className="sticky top-0 z-30 bg-surface-container-low border-b border-outline-variant/30 px-lg py-sm flex items-center justify-between">
@@ -51,6 +65,11 @@ export default function DashboardHeader() {
             </p>
           </div>
         </div>
+        <button className="cursor-pointer flex md:hidden hover:text-red-500 transition-colors duration-300" onClick={handleLogout}>
+          <span className="material-symbols-outlined">
+            logout
+          </span>
+        </button>
 
       </div>
     </header>
